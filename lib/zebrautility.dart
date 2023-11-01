fade48eb8920bf1163be3641c0dd73fe7716541e
Sarayut Kungsaranuwat <sarayut.kungsaranuwat@gmail.com>
2023-11-01 17:13:41 +0700

    Update zebrautility.dart

--- lib/zebrautility.dart
+++ lib/zebrautility.dart
@@ -5,17 +5,22 @@ import 'package:zebrautility/ZebraPrinter.dart';
 class Zebrautility {
   static const MethodChannel _channel = const MethodChannel('zebrautility');
 
-  static Future<ZebraPrinter> getPrinterInstance(
-      {Function(String name, String ipAddress, bool isWifi)? onPrinterFound,
+  static Future<ZebraPrinter> getPrinterInstance({
+    Function(String name, String ipAddress, bool isWifi)? onPrinterFound,
+    onPrinterDiscoveryDone,
+    Function(int errorCode, String errorText)? onDiscoveryError,
+    Function(String status, String color)? onChangePrinterStatus,
+    onPermissionDenied,
+  }) async {
+    String id = await _channel.invokeMethod("getInstance");
+    ZebraPrinter printer = ZebraPrinter(
+      id,
+      onPrinterFound,
       onPrinterDiscoveryDone,
-      Function(int errorCode, String errorText)? onDiscoveryError,
-      Function(String status, String color)? onChangePrinterStatus,
-      onPermissionDenied}) async {
-    String id =
-        await _channel.invokeMethod("getInstance");
-    ZebraPrinter printer = ZebraPrinter(id, onPrinterFound,
-        onPrinterDiscoveryDone, onDiscoveryError, onChangePrinterStatus,
-        onPermissionDenied: onPermissionDenied);
+      onDiscoveryError,
+      onChangePrinterStatus,
+      onPermissionDenied: onPermissionDenied,
+    );
     return printer;
   }
 }
